e6b97bee62ba4c6f3783e795d5bb18c30cf16883
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-07-07 19:51:49 -0400

    Started inquirer prompts. Choose employee's role.

--- app.js
+++ app.js
@@ -14,7 +14,20 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-function 
+const userQuestions = [
+    {
+        type: "list",
+        name: "role",
+        message: "What is your employee's position?",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern",
+        ]
+    },
+]
+
+inquirer.prompt(userQuestions)
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
